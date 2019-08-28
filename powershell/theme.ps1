187bd6181f51283f6610de20029f51cf80f5d798
hidek <23580942+hidejack@users.noreply.github.com>
2019-08-28 15:30:49 +0800

    first commit

---
+++ powershell/theme.ps1
@@ -0,0 +1,15 @@
+Import-Module posh-git
+Import-Module oh-my-posh
+Set-Theme Sorin
+Import-Module PSColor
+Import-Module DirColors
+Screenfetch
+
+function cd... { Set-Location ..\.. }
+function cd.... { Set-Location ..\..\.. }
+function cd..... { Set-Location ..\..\..\.. }
+function cd...... { Set-Location ..\..\..\..\.. }
+function cd....... { Set-Location ..\..\..\..\..\.. }
+function cd........ { Set-Location ..\..\..\..\..\..\.. }
+function cd......... { Set-Location ..\..\..\..\..\..\..\.. }
+
